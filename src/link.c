eddbabbaff0bfa54042de3495e7f499c50207747
a79089 <a79089@alunos.uminho.pt>
2017-07-27 15:26:16 +0100

    replaced most code with a simple call to cat

--- src/link.c
+++ src/link.c
@@ -12,30 +12,23 @@
  */
 
 int main(int argc,char** argv){
-	if(argc != 3){
-		fprintf(stderr,"Usage: ./link <processId1> <processId2>\n");
-		exit(-1);
-	}
+    if(argc != 3){
+        fprintf(stderr,"Usage: ./link <processId1> <processId2>\n");
+        exit(-1);
+    }
 
-	int statusR = open(argv[1],O_RDONLY);
-	int statusW = open(argv[2],O_WRONLY);
+    int statusR = open(argv[1],O_RDONLY);
+    int statusW = open(argv[2],O_WRONLY);
 
-	if(statusR == -1){ fprintf(stderr,"[ERROR]opening read pipe %s, write pipe was %s\n",argv[1],argv[2]); exit(-1); }
-	if(statusW == -1){ fprintf(stderr,"[ERROR]opening write pipe %s\n",argv[2]); exit(-1); }
+    if(statusR == -1){ fprintf(stderr,"[ERROR]opening read pipe %s, write pipe was %s\n",argv[1],argv[2]); exit(-1); }
+    if(statusW == -1){ fprintf(stderr,"[ERROR]opening write pipe %s\n",argv[2]); exit(-1); }
 
-	/* read from A while possible, and write to B */
+    /* read from A while possible, and write to B */
 
-	char buffer[PIPE_BUF]; clean_buffer(buffer,PIPE_BUF);
-	int statusReadBuffer;
-	int statusWriteBuffer;
+    dup2(statusR,0); close(statusR);
+    dup2(statusW,1); close(statusW);
 
-	while( (statusReadBuffer = readln(statusR,buffer,sizeof(buffer))) > 0){
-		if(statusReadBuffer < 0){ perror("Error reading from first processes's output\n"); exit(-1); }
+    execlp("cat","cat",NULL);
 
-		statusWriteBuffer =	write(statusW,buffer,strlen(buffer));
-		if(statusWriteBuffer < 0){ perror("Error writing to second processes's input\n"); exit(-1); }
-    clean_buffer(buffer,PIPE_BUF);
-	}
-
-	return 0;
+    return 0;
 }
