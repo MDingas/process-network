77d7c9289c256333bab14daf01c1f244ee3823f9
a79089 <a79089@alunos.uminho.pt>
2017-08-05 01:09:23 +0100

    simple parsing programs for example network

---
+++ src/isFemale.c
@@ -0,0 +1,17 @@
+#include "genLibrary.h"
+
+int main(int argc, char** argv){
+    char buffer[PIPE_BUF]; clean_buffer(buffer,PIPE_BUF);
+
+    while(1){
+        while(readln(0,buffer,sizeof(buffer)) > 0){
+            remove_newline(buffer);
+            if(prefix_match("F,",buffer) == 1){
+                add_newline(buffer);
+                if(write(1,buffer,strlen(buffer)) == -1){ error_writing_terminal(); }
+                clean_buffer(buffer,sizeof(buffer));
+            }
+
+        }
+    }
+}
