7b0208bc96cd703b34322e53ea850f353658a62a
a79089 <a79089@alunos.uminho.pt>
2017-08-05 01:07:53 +0100

    now allows space separated words to be added

--- src/const.c
+++ src/const.c
@@ -8,25 +8,33 @@
  */
 
 int main(int argc,char** argv){
-      if(argc != 2){
-            perror("usage: ./const <value to append>");
-            exit(-1);
-      }
+    if(argc < 2){
+        perror("usage: ./const <value to append>");
+        exit(-1);
+    }
 
-      char buffer[PIPE_BUF]; clean_buffer(buffer,PIPE_BUF);
+    char buffer[PIPE_BUF]; clean_buffer(buffer,PIPE_BUF);
+    char constValue[200]; clean_buffer(constValue,200);
 
-      while(1){
-            while(readln(0,buffer,sizeof(buffer)) > 0){
-                  remove_newline(buffer);
-                  strcat(buffer,":");
-                  strcat(buffer,argv[1]);
-                  add_newline(buffer);
-                  if(write(1,buffer,strlen(buffer)) == -1){
-                      error_writing_terminal();
-                  }
-                  clean_buffer(buffer,sizeof(buffer));
+    for (int i = 1; i < argc; i++) {
+        strcat(constValue,argv[i]);
+        if (i != argc - 1){
+            strcat(constValue," ");
+        }
+    }
+
+    while(1){
+        while(readln(0,buffer,sizeof(buffer)) > 0){
+            remove_newline(buffer);
+            strcat(buffer,":");
+            strcat(buffer,constValue);
+            add_newline(buffer);
+            if(write(1,buffer,strlen(buffer)) == -1){
+                error_writing_terminal();
             }
-      }
+            clean_buffer(buffer,sizeof(buffer));
+        }
+    }
 
-      return 0;
+    return 0;
 }
